20fdc51a24aacbd6cbd273d0a0caef988f8f0708
Camus Qiu <camus@aion.network>
2019-05-05 18:58:37 +0800

    other: fetch jni jar, from temp repo of camus

--- vms/avm/build.rs
+++ vms/avm/build.rs
@@ -13,13 +13,23 @@ fn main() {
 
     println!("cargo:rustc-link-search=native={}", outdir);
     println!("cargo:rustc-link-lib=static=avmjni");
+
+    // fetch jni jar
+    let mut jni_jar_path = env!("CARGO_MANIFEST_DIR").to_string();
+    jni_jar_path.extend("/libs/aion_vm/org-aion-avm-jni.jar".chars());
+    println!("{:?}", jni_jar_path);
+    Command::new("wget")
+            .arg("https://github.com/aion-camus/rust_avm/releases/download/v0.5.0/org-aion-avm-jni.jar")
+            .args(["-O", &jni_jar_path].iter())
+            .status()
+            .expect("fetch jni jar error");
     
     // fetch avm libs
     Command::new("wget")
             .arg("https://github.com/aionnetwork/AVM/archive/1.0.tar.gz")
             .args(["-O", "/tmp/avm.tar.gz"].iter())
             .status()
-            .expect("update AVM error");
+            .expect("fetch AVM error");
 
     // unpack avm package and put the jars in libs dir
     Command::new("tar")
