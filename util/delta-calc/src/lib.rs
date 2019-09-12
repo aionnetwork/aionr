53746c7ffafa9e1ff660c31653f536c0e977765b
lyj729047457 <yuanjun@aion.network>
2019-09-12 17:37:29 +0800

    other: delta calc ut

--- util/delta-calc/src/lib.rs
+++ util/delta-calc/src/lib.rs
@@ -63,9 +63,44 @@ pub fn calculate_delta(difficulty: U256, seed: &[u8], stake: BigUint) -> u64 {
 #[cfg(test)]
 mod test {
     use super::*;
+    fn ln_sub(hash_seed: &BigUint) -> Result<f64, String> {
+        // convert BigUint to little endian bytes
+        let hash_seed_bytes: Vec<u8> = hash_seed.to_bytes_le();
+
+        // this is the length of the bytes we need to convert to double
+        // we do not need full precision of BigInt, for two reasons:
+        // 1.double only have 53-bit precision
+        // 2.precision after 53-bit has little effect on the results after ln()
+        const BYTES: usize = 12;
+
+        // convert hash_seed_bytes to eps*(256)^l where 1 < eps < 256 , l = bytes_length - 1
+        let start = if hash_seed_bytes.len() < BYTES {
+            0
+        } else {
+            hash_seed_bytes.len() - BYTES
+        };
+        let mut eps_hash_seed: f64 = 0.0;
+        for i in start..hash_seed_bytes.len() {
+            eps_hash_seed = eps_hash_seed / 256f64 + (hash_seed_bytes[i] as f64);
+        }
+
+        // calculate ln(256)
+        let ln_256: f64 = (256f64).ln();
+
+        // return  (33 - l_hash_seed) * ln(256) - ln(eps_hash_seed)
+        // 1 is eps of 2^256, and 33 is the length of 2^256
+        // 2^256 = (2^8)^32, so it has a '1' and 32 '0's, so its length is 33
+        Ok(ln_256 * ((33 - hash_seed_bytes.len()) as f64) - eps_hash_seed.ln())
+    }
+
     #[test]
     fn test_calculate_delta() {
-
-        //        assert_eq!(calculate_delta());
+        let seed = [2u8; 64];
+        let hash_of_seed = blake2b(&seed[..]);
+        let ln_sub = ln_sub(&hash_of_seed.into()).unwrap();
+        assert_eq!(
+            calculate_delta(U256::from(10000u64), &seed, BigUint::from(333u64)),
+            (10000f64 * ln_sub / 333f64) as u64
+        );
     }
 }
