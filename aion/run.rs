a684ec5dd2919cd894b6d2a6b26eed60fee1fd8e
lyj729047457 <yuanjun@aion.network>
2020-01-13 14:34:03 +0800

    other: cleanup aion module

--- aion/run.rs
+++ aion/run.rs
@@ -397,9 +397,7 @@ fn print_running_environment(
         info!(
             target: "run",
             " config path: {}",
-            Colour::White
-                .bold()
-                .paint(config)
+            config
         );
     } else {
         info!(target: "run", "Start without config.");
@@ -412,25 +410,19 @@ fn print_running_environment(
             info!(
                 target: "run",
                 "genesis path: {}",
-            Colour::White
-                .bold()
-                .paint(absolute(filename.to_string()))
+                absolute(filename.to_string())
             );
         }
     }
     info!(
         target: "run",
         "   keys path: {}",
-        Colour::White
-            .bold()
-            .paint(dirs.keys_path(spec_data_dir).to_string_lossy().into_owned())
+        dirs.keys_path(spec_data_dir).to_string_lossy().into_owned()
     );
     info!(
         target: "run",
         "     db path: {}",
-        Colour::White
-            .bold()
-            .paint(db_dirs.db_root_path().to_string_lossy().into_owned())
+        db_dirs.db_root_path().to_string_lossy().into_owned()
     );
 }
 
@@ -445,7 +437,7 @@ fn print_logo() {
         Colour::Blue.bold().paint("| |__| |   / ____ \\   | |\\  |\n"),
         Colour::Blue.bold().paint(" \\____/   /_/    \\_\\  |_| \\_|\n\n")
     );
-    info!(target: "run","       build: {}", Colour::White.bold().paint(version()));
+    info!(target: "run","       build: {}", version());
 }
 
 fn prepare_account_provider(
